9f514e6c58723c44674abf764c624817b6022ee8
st5963 <info@standard-c.com>
2017-08-24 02:17:26 +0900

    delete
    delete

--- reb/wp-content/plugins/lightning-advanced-unit/README.md
+++
@@ -1,5 +0,0 @@
-# lightning-advanced-unit
-
-Lightning 専用の機能拡張プラグイン  
-
-Lightning 以外でも使うようなものは ExUnitに実装する
--- reb/wp-content/themes/angel_tcd047/searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-  <div>
-    <label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-    <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-    <input type="submit" id="searchsubmit" value="&#xe915;" />
-  </div>
-</form>
